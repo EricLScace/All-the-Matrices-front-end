1a76cf0be3a93353b3f4f170da573e367f0254e8
EricLScace <mail@ericscace.com>
2017-06-13 23:05:09 -0400

    Wrong branch: added User object for authN
    User is not referenced but in 1 spot to date.
    Prep for registration.

--- assets/scripts/user.js
+++ assets/scripts/user.js
@@ -1,11 +1,46 @@
 'use strict'
 
-const User = function () {
-  // public property: isLoggedIn initializes as false
+// Define User constructor
+const User = function (isLoggedIn, email, password, id, authNToken, name, org) {
+  this.setLogInStatus(isLoggedIn, email, password, id, authNToken, name, org)
 }
 
-User.prototype.isLoggedIn = function () {
-  return false
+User.prototype.setLogInStatus = function (isLoggedIn, email, password, id, authNToken, name, org) {
+  // note: email & password are used as login credentials.
+  // This function is both constructor and a public method.
+  // Three forms of this function:
+  // _setLogInStatus(true, …) to record a log-in & returns true.
+  // _setLogInStatus(false) logs out the player; returns false
+  // _setLogInStatus(null, email, password) to cache a name and
+  //    password (received as strID).
+  //    Used if we want to re-authenticate user in the immediate future; e.g.,
+  //    to log a new user in after he was successfully registered.
+  if (arguments.length > 0) {
+    switch (isLoggedIn) {
+      case true:
+        // Accept proffered credentials as logged in.
+        this._isLoggedIn = true
+        this.id = id
+        this.authNToken = authNToken
+        this.name = name
+        this.org = org
+        break
+      case null:
+        // Cache proffered email & password for future use
+        this._isLoggedIn = null
+        this.email = email
+        this._password = password
+        break
+      case false:
+        // Log out a player by changing _isLoggedIn and erasing credentials.
+        this._isLoggedIn = false
+        this.email = ''
+        this.id = ''
+        this.authNToken = ''
+        this._password = ''
+    }
+  }
+  return this._isLoggedIn
 }
 
 module.exports = User
