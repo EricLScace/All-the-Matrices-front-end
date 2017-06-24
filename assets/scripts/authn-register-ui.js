486a6790e5f575f805e5135cf1cb5a39cfd133f4
EricLScace <mail@ericscace.com>
2017-06-24 15:28:01 -0400

    Add register form submit handler
    Consequences:
    * Register form submits OK.
    * Wrong error message if duplicate registration.
    * Surplus period after emoticon in mysterious failure message
    
    WIP: issue #88

--- assets/scripts/authn-register-ui.js
+++ assets/scripts/authn-register-ui.js
@@ -3,34 +3,33 @@
 const announceUI = require('./announce-ui.js')
 const authnAPI = require('./authn-api')
 const getFormFields = require('../../lib/get-form-fields')
-const logInRegisterButtons = require('../templates/logInRegisterButtons.handlebars')
 const msg = require('./messages.js')
 const registerForm = require('../templates/registerForm.handlebars')
 const store = require('./store')
-const authnLoginUI = require('./authn-login-ui')
 
-// Log-in & register buttons
-const loadLogInRegisterButtons = function () {
-  $('#authn').html(logInRegisterButtons())
-  // $('#log-in').on('click', onLogInClick)
-  $('#register').on('click', onRegisterClick)
-  $('#log-in').on('click', authnLoginUI.onLogInClick)
+const failure = function (response) {
+  if (response.responseText.email === 'has already been taken') {
+    announceUI.post(msg.alreadyRegistered, 'announcement')
+    // Try logging in
+  } else {
+    announceUI.post(msg.registrationFailed, 'announcement')
+  }
 }
 
 // Submitted the register form
-const onRegister = function (e) {
+const onSubmit = function (e) {
   e.preventDefault()
   // Clear old error messages, if any.
   announceUI.clear('announcement')
-  const credentials = getFormFields(e.target)
+  const credentials = getFormFields(e.target.form)
   if (validateCredentials(credentials)) {
     // Heroku can be slow; indicate registering.
     announceUI.post(msg.registering)
     // Cache credentials & launch API request
     store.user.setLogInStatus(null, credentials.email, credentials.password, null, null, credentials.name, credentials.org)
     authnAPI.register(credentials)
-      .then(registerSuccess)
-      .catch(registerFailure)
+      .then(success)
+      .catch(failure)
   }
 }
 
@@ -41,16 +40,7 @@ const onRequest = function () {
   $('#authn').html(registerForm)
 }
 
-const registerFailure = function (response) {
-  if (response.responseText.email === 'has already been taken') {
-    announceUI.post(msg.alreadyRegistered, 'announcement')
-    // Try logging in
-  } else {
-    announceUI.post(msg.registrationFailed, 'announcement')
-  }
-}
-
-const registerSuccess = function (response) {
+const success = function (response) {
   announceUI.post(msg.registeredOK, 'announcement')
 }
 
@@ -72,6 +62,6 @@ const validateCredentials = function (credentials) {
 }
 
 module.exports = {
-  loadLogInRegisterButtons,
+  onSubmit,
   onRequest
 }
